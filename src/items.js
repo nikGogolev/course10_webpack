642da86c22619ee1656d9906aec035dbfc8c6373
nv_gogolev <78842306+nikGogolev@users.noreply.github.com>
2022-02-15 20:48:00 +0300

    lesson3Hw

---
+++ src/items.js
@@ -0,0 +1,59 @@
+import image1 from '../media/images/1.jpg';
+import image2 from '../media/images/2.jpg';
+import image3 from '../media/images/3.jpg';
+import image4 from '../media/images/4.gif';
+
+import audio1 from '../media/audio/1.mp3';
+import audio2 from '../media/audio/2.mp3';
+import audio3 from '../media/audio/3.mp3';
+
+import video1 from '../media/video/1.mp4';
+import video2 from '../media/video/2.mp4';
+
+export default [
+  {
+    resourse: image1,
+    type: 'img',
+    title: 'Picture 1'
+  },
+  {
+    resourse: image2,
+    type: 'img',
+    title: 'Picture 2'
+  },
+  {
+    resourse: image3,
+    type: 'img',
+    title: 'Picture 3'
+  },
+  {
+    resourse: image4,
+    type: 'img',
+    title: 'Picture 4'
+  },
+  {
+    resourse: audio1,
+    type: 'audio',
+    title: 'Audio 1'
+  },
+  {
+    resourse: audio2,
+    type: 'audio',
+    title: 'Audio 2'
+  },
+  {
+    resourse: audio3,
+    type: 'audio',
+    title: 'Audio 3'
+  },
+  {
+    resourse: video1,
+    type: 'video',
+    title: 'Video 1'
+  },
+  {
+    resourse: video2,
+    type: 'video',
+    title: 'Video 2'
+  }
+];
