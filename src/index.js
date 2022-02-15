951089cb31ae7850c5230fd84dbb51a539545cda
nv_gogolev <78842306+nikGogolev@users.noreply.github.com>
2022-02-15 20:46:39 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import("./main.scss");
+import { drawGalleryItem } from './item';
+import items from './items';
+
+const galleryRootElement = document.getElementById('gallery');
+items.map(item => galleryRootElement.appendChild(drawGalleryItem(item)));
